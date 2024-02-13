f8276ca1066b3f399d209f62901db2b8dee4df37
ferry-wijaya <ferrywijaya1986@gmail.com>
2024-02-13 17:25:55 +0700

    Setup Angular Frontend (SSR) Packages

--- frontend/blitzmart-fesf-ssr/src/app/app.module.ts
+++ frontend/blitzmart-fesf-ssr/src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
@@ -10,10 +12,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [
-    provideClientHydration()
+    provideClientHydration(),
+    provideAnimationsAsync()
   ],
   bootstrap: [AppComponent]
 })
